b3772c32144b919bd5d49a5b64355d4c7af8c6b7
LeonEidelman <leoneidelman09@gmail.com>
2025-07-24 14:01:40 +0300

    toast functionality implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Route, Routes } from 'react-router';
+import HomePage from '../pages/HomePage';
+import CreatePage from '../pages/CreatePage';
+import NoteEditorPage from '../pages/NoteEditorPage';
+import toast from 'react-hot-toast';
 
 const App = () => {
-  return <div>rere</div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          toast.success('congrats');
+        }}
+      >
+        Click me
+      </button>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/create"
+          element={<CreatePage />}
+        />
+        <Route
+          path="/note/:id"
+          element={<NoteEditorPage />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
