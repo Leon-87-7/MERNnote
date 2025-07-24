e963f8359c04b81e0a3601d0504733828347f698
LeonEidelman <leoneidelman09@gmail.com>
2025-07-24 16:39:03 +0300

    add a test card from daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,25 @@ const App = () => {
       <button className="btn btn-accent">Accent</button>
       <button className="btn btn-ghost">Ghost</button>
       <button className="btn btn-link">Link</button>
+      <br />
+      <br />
+      <br />
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
       <Routes>
         <Route
           path="/"
