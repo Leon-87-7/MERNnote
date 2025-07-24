88a5def26f6380e543526a6c1f42e87ad30156db
LeonEidelman <leoneidelman09@gmail.com>
2025-07-24 16:32:52 +0300

    daisyUI-forest theme init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,15 @@ import NoteEditorPage from '../pages/NoteEditorPage';
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="forest">
+      <button className="btn btn-primary">click-me</button>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
       <Routes>
         <Route
           path="/"
