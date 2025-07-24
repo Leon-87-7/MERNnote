9e0dddb943f53c791039d89b8110e130913b0704
LeonEidelman <leoneidelman09@gmail.com>
2025-07-24 09:02:35 +0300

    ratelimit initialize

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import noteRoutes from './routes/notesRoutes.js';
 import { connectDB } from '../config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from '../middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -12,11 +13,12 @@ connectDB();
 //middleware
 app.use(express.json()); //this middleware wilt parse JSON bodies: req.body
 
-/* our simple custom middleware
+app.use(rateLimiter);
+// our simple custom middleware
 app.use((req, res, next) => {
   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
   next();
-});*/
+});
 
 app.use('/api/notes', noteRoutes);
 
