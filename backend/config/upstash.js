5c97ae0c6bec35d724e97de9d44c6c3182705804
LeonEidelman <leoneidelman09@gmail.com>
2025-07-24 13:34:27 +0300

    rateLimiter tested

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -5,10 +5,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// create a ratelimiter that allows 10 req per 20 seconds
+// create a ratelimiter that allows 100 req per 60 seconds
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, '60 s'),
+  limiter: Ratelimit.slidingWindow(100, '60 s'),
 });
 
 export default ratelimit;
--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -3,6 +3,8 @@ import ratelimit from '../config/upstash.js';
 const rateLimiter = async (req, res, next) => {
   try {
     const { success } = await ratelimit.limit('my-limit-key');
+    /*'my-limit-key' is now for all the users but it should be for each user
+     when we have an authentication and it will be on the userId*/
 
     if (!success) {
       return res.status(429).json({
