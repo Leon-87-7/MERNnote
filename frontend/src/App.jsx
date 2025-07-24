886e3f3034adfe9916d793d0563aef2105a45b58
LeonEidelman <leoneidelman09@gmail.com>
2025-07-24 16:40:43 +0300

    cleanup for build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,34 +5,7 @@ import NoteEditorPage from '../pages/NoteEditorPage';
 
 const App = () => {
   return (
-    <div data-theme="sunset">
-      <button className="btn btn-primary">click-me</button>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-      <br />
-      <br />
-      <br />
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+    <div>
       <Routes>
         <Route
           path="/"
