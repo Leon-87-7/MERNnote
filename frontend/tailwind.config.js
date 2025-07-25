8ca22ea002e1c3dd9cfe0cff737f4aaac313e3df
LeonEidelman <leoneidelman09@gmail.com>
2025-07-25 22:41:49 +0300

    successfully implemented note creation
    and fixed the tailwind not reading the content properly

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,16 +2,21 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  safelist: [
-    'grid',
-    'grid-cols-1',
-    'grid-cols-2',
-    'grid-cols-3',
-    'md:grid-cols-2',
-    'lg:grid-cols-3',
-    'gap-6',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{js,jsx,ts,tsx}', // Nested folders
+    './pages/**/*.{js,jsx,ts,tsx}', // If you have a pages folder
   ],
+  // safelist: [
+  //   'grid',
+  //   'grid-cols-1',
+  //   'grid-cols-2',
+  //   'grid-cols-3',
+  //   'md:grid-cols-2',
+  //   'lg:grid-cols-3',
+  //   'gap-6',
+  // ],
   theme: {
     extend: {},
   },
