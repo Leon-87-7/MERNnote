fae04e650469740082356beceb90e1c61323e962
LeonEidelman <leoneidelman09@gmail.com>
2025-07-28 16:11:34 +0300

    using custom UI for Auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,20 @@
 import { Route, Routes } from 'react-router';
-import * as reactRouterDom from 'react-router';
-import { getSuperTokensRoutesForReactRouterDom } from 'supertokens-auth-react/ui';
 import { SessionAuth } from 'supertokens-auth-react/recipe/session';
-import { EmailPasswordPreBuiltUI } from 'supertokens-auth-react/recipe/emailpassword/prebuiltui';
-import { ThirdPartyPreBuiltUI } from 'supertokens-auth-react/recipe/thirdparty/prebuiltui';
 
 import HomePage from '../pages/HomePage';
 import CreatePage from '../pages/CreatePage';
 import NoteEditorPage from '../pages/NoteEditorPage';
+import AuthUI from './components/AuthUI';
 
 const App = () => {
   return (
     <div>
       <div className="absolute -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_65%,#15843c_150%)]" />
       <Routes>
-        {/* SuperTokens auth routes*/}
-        {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [
-          EmailPasswordPreBuiltUI,
-          ThirdPartyPreBuiltUI,
-        ])}
+        <Route
+          path="/auth"
+          element={<AuthUI />}
+        />
         <Route
           path="/"
           element={<HomePage />}
